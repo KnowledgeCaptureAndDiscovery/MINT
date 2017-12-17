6b0e8b3cc245996f1cb5baee934974866ac5e541
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2017-12-16 19:37:29 -0600

    new queries

--- catalog_hydrology/querytest.py
+++ catalog_hydrology/querytest.py
@@ -3,7 +3,7 @@ g = rdflib.Graph()
 
 g.parse("rdf_output.ttl", format="n3")
 
-# Give me all the respository
+#Give me all the respository
 #qres = g.query(
 #	""" SELECT DISTINCT ?a ?b ?c
 #	    WHERE {
@@ -31,27 +31,48 @@ g.parse("rdf_output.ttl", format="n3")
 #		HAVING(COUNT(?recurso) > 0) 
 #	""")	
 
-#Give the number of locations from the catalog
+#Give me the number of locations from the catalog
 #qres = g.query(
 #	""" SELECT (COUNT(?l) AS ?nl) 
 #	WHERE {
 #		     ?recurso a <http://www.w3.org/ns/dcat#Dataset>.
 #		     ?recurso <http://purl.org/dc/terms/spatial> ?l.
 #		}""")	
-#Give the publisher with the most published resources
+
+#Give me the publisher with the most published resources
+#qres = g.query(
+#	""" SELECT ?p (COUNT(?r) AS ?nr) 
+#		WHERE {
+#		     ?r a <http://www.w3.org/ns/dcat#Dataset>.
+#		     ?r <http://purl.org/dc/terms/publisher> ?p.
+#		}
+#		GROUP BY ?p
+#		ORDER BY DESC (?nr)
+#		LIMIT 1
+#		""")	
+
+#Give me all the organizations that published resources
+#qres = g.query(
+#	""" SELECT DISTINCT ?no
+#		WHERE{
+#		 ?o a <http://xmlns.com/foaf/0.1/Organization>.
+#		 ?o <http://xmlns.com/foaf/0.1/name> ?no
+#		}
+#	""") 
+#Give me all authors that published resources
 qres = g.query(
-	""" SELECT ?p (COUNT(?r) AS ?nr) 
-		WHERE {
-		     ?r a <http://www.w3.org/ns/dcat#Dataset>.
-		     ?r <http://purl.org/dc/terms/publisher> ?p.
+	""" SELECT DISTINCT ?na
+		WHERE{
+		 ?a a <http://xmlns.com/foaf/0.1/Person>.
+		 ?a <http://xmlns.com/foaf/0.1/name> ?na
 		}
-		GROUP BY ?p
-		ORDER BY DESC (?nr)
-		LIMIT 1
-		""")	
-		
+	""") 
 
+#Give me the organization that published most resources
 
+#Give me the author that published most resources	
+
+#Give me all the authors from organization BGS
 
 for row in qres:
-   print("%s %s" % row)
+   print("%s " % row)
